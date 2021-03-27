171f83875b6a69bc3171f5d2fab5ad52f7bca58a
satish mungade <63844818+satishmungade@users.noreply.github.com>
2021-03-27 19:13:18 +0530

    Add files via upload

---
+++ GNU_32Bit_Assembly/Global_Initilized_variable.s
@@ -0,0 +1,28 @@
+.section .rodata
+	msg_main_print1:
+	.string "\n Global iNo is :%d"
+
+.section .data   			#global variable store in Data initilized
+	.globl g_iNo			#name g_iNo
+	.type g_iNo ,@object	#object tail its variable
+	.size g_iNo, 4			#size for variale which you want 
+	.align 4				#align for variable my mschin 32bit thats why i give 4(segment)
+	g_iNo:					#labal
+	.int 20					#hear use Assembly directives start with .(Dot) .int value give 20 
+
+.section .text
+.globl main
+.type main ,@function
+main:
+		
+		pushl %ebp				#push a ebp in stack frame
+		movl %esp , %ebp		#esp value move in ebp
+
+		movl g_iNo,%eax			#if we have name than we can access directly g_iNo value move on eax register
+		pushl %eax				#eax register push
+		pushl $msg_main_print1
+		call printf				#call printf
+		addl $8,%esp			#back for 8(g_iNo + .String)
+
+		pushl $0
+		call exit
