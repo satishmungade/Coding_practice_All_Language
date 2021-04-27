44014057e91fd3bf2f599d5271642c6367855f08
satish mungade <63844818+satishmungade@users.noreply.github.com>
2021-04-27 22:01:37 +0530

    Delete GNU_32Bit_Assembly directory

--- GNU_32Bit_Assembly/1_External_Linkage/two.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int g_iNo = 10;
-void fun(void)
-{
-	printf("\n in fun g_iNo :%d",g_iNo);
-}
--- GNU_32Bit_Assembly/UnInitialized_Global_variable/Default_Value_Global_variable.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-int g_iNo;
-int main(void)
-{
-	printf("\n Number is  %d",g_iNo);
-	exit(0);
-}
