aafd8cdc50b9c2212965c610664bad22851c2527
satish mungade <63844818+satishmungade@users.noreply.github.com>
2021-05-13 00:23:45 +0530

    Update Structure_Comparison_Local_Global_Object_p.s

--- GNU_32_Bit_Assembly/Structure/Structure_Comparison_Local_Global_Object_p.s
+++ GNU_32_Bit_Assembly/Structure/Structure_Comparison_Local_Global_Object_p.s
@@ -24,22 +24,23 @@
 	.string "obj2 & obj3 Comparison is :\n "
 
 	msg_main_Equal:
-	.string " Object is Equal \n"
+	.string "\n\t Object is Equal \n\n"
 
 	msg_main_NotEqual:
-	.string "Object Are Not Equal \n"
+	.string "\n\t object Are Not Equal \n\n"
 
 
 .section .data		
 	.globl obj1
-	.type obj1,@object
-	.size obj1,12
+	.type  obj1,@object
+	.size  obj1,12
 	.align 4
-	obj1 :
-		.ascii "A"
-		.zero 3
-		.int 10
-		.value 20
+			obj1 :
+			.ascii "A"
+			.zero 3
+			.int 20
+			.value 30
+			.zero 2
 
 .section .text
 .globl main		
@@ -48,14 +49,14 @@
 		pushl	%ebp
 		movl 	%esp,%ebp
 
-		subl $24,%esp
-		movl $'B',-12(%ebp)
-		movl $30,-8(%ebp)
-		movl $40,-4(%ebp)
+		subl 	$24,%esp
+		movl 	$'B',-12(%ebp)
+		movl 	$30,-8(%ebp)
+		movl 	$40,-4(%ebp)
 
-		movl $'B',-24(%ebp)
-		movl $30,-20(%ebp)
-		movl $40,-16(%ebp)
+		movl 	$'B',-24(%ebp)
+		movl 	$30,-20(%ebp)
+		movl 	$40,-16(%ebp)
 
 
 		pushl	$msg_main_obj1_print
@@ -115,35 +116,69 @@
 		call 	printf
 		addl	$4,%esp
 
-		movl 	$obj1,%ebx
-		movl    -12(%ebp),%eax
-		cmpl   %eax,%ebx
-		je		Lebal_Equal 
 		
-Lebal_NotEqual:
-	pushl   msg_main_NotEqual
-	call    printf
-	addl 	$4,%esp
-	jmp Label_Exit
+		movl    -12(%ebp),%edx		#Local object reverse order -12 -8 -4 same like local variable
+		movl 	$obj1,%ebx
+		movl 	(%ebx),%eax			#first Member object strat la
+		cmpl	%edx,%eax			
+		jne		Lebal_Not_Equal
+
+		movl 	-8(%ebp),%edx
+		movl 	4(%ebx),%eax		#object pasun 4la 2nd ahe 
+		cmpl	%edx,%eax
+		jne		Lebal_Not_Equal
+
+		movl 	-4(%ebp),%edx
+		movl 	8(%ebx),%eax		#object pasun 8 la 3rd ahe 
+		cmpl	%edx,%eax
+		jne		Lebal_Not_Equal
+
+		pushl	$msg_main_Equal
+		call	printf
+		addl	$4,%esp
 
 
-Lebal_Equal:
-	movl  $obj1,%ebx
-	movl  8(%ebx),%eax
-	movl  -8(%ebp),%edx
-	cmpl  %edx,%eax
-	jl	Lebal_NotEqual
+		jmp		Lebal_Check_Onther_Obj #if equal than jump direct onther object checking
 
-	movl  $obj1,%ebx
-	movl 4(%ebx),%eax
-	movl -4(%ebp),%edx
-	cmpl  %edx,%eax
-	jl 	Lebal_NotEqual
-
-	pushl $msg_main_Equal
-	call printf
-	addl $4,%esp
+Lebal_Not_Equal:
 	
-Label_Exit:		
-	pushl	$0
-	call 	exit
+	pushl	$msg_main_NotEqual
+	call 	printf
+	addl	$4,%esp
+
+Lebal_Check_Onther_Obj:
+
+	pushl	$msg_main_comparison_obj2_obj3
+	call	printf
+	addl	$4,%esp	
+
+	movl 	-12(%ebp),%eax
+	movl 	-24(%ebp),%edx
+	cmpl 	%edx,%eax
+	jne		Lebal_Not_Equal2
+
+	movl 	-8(%ebp),%eax
+	movl    -20(%ebp),%edx
+	cmpl	%edx,%eax
+	jne		Lebal_Not_Equal2
+
+	movl 	-4(%ebp),%eax
+	movl 	-16(%ebp),%edx
+	cmpl	%edx,%eax
+	jne		Lebal_Not_Equal2
+
+	pushl	$msg_main_Equal
+	call	printf
+	addl	$4,%esp
+	jmp 	Lebal_Exit
+
+Lebal_Not_Equal2:
+	pushl	$msg_main_NotEqual
+	call 	printf
+	addl	$4,%esp	
+
+Lebal_Exit:
+		pushl	$0
+		call 	exit
+
+
