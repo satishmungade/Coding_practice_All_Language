96410f5c778c92f933947784e45fe639bd3f2d95
satish mungade <63844818+satishmungade@users.noreply.github.com>
2021-05-15 09:00:23 +0530

    Update 1D_Array_Dynamic_p.s

--- GNU_32_Bit_Assembly/Array/1D_Array_Dynamic_p.s
+++ GNU_32_Bit_Assembly/Array/1D_Array_Dynamic_p.s
@@ -20,46 +20,55 @@
 	msg_main_output_elements_print:
 	.string "\n index is :%d Element  is :%d \n "
 
+.section .data
+	.globl 	Pptr
+	.type	Pptr,@object
+	.size 	Pptr,4
+	.align	4
+	Pptr:
+		.int 0
+
 .section .text
-.globl main 	
+.globl main 
 .type main,@function
 	main:
-		pushl 	%ebp
-		movl	%esp,%ebp
 
-		subl	$12,%esp
+		pushl %ebp
+		movl %esp,%ebp
 
-		movl	$0,-12(%ebp)
+		subl $8,%esp
 
 		pushl	$msg_main_input_print
-		call	printf
+		call 	printf
 		addl	$4,%esp
 
-		leal	-4(%ebp),%ebx
+		leal 	-4(%ebp),%ebx
+		
 		pushl	%ebx
-		pushl	$msg_main_input_scan
-		call	scanf
+		pushl	$msg_main_input_elements_scan
+		call 	scanf
 		addl	$8,%esp
 
-		movl	-4(%ebp),%eax
-		movl	$4,%ecx
-		mul		%ecx
-
-		pushl	%eax
-		call	malloc
-		addl	$4,%esp
-		cmpl	$0,%eax
-		jne		Label_Not_failed
-
+		movl 	-4(%ebp),%eax
+		movl  	$4,%ecx
+		mul	  	%ecx
+		
+		pushl 	%eax
+		call  	malloc
+        movl    %eax,Pptr       #nava madhe adrress takch
+        addl    $4,%esp
+		cmpl 	$0,%eax
+		jne 	Label_memery_Not_Failed
+		
 		pushl	$msg_main_Memory_failed
 		call	printf
 		addl	$4,%esp
 
 		pushl	$-1
-		call	exit
+		call 	exit
 
-Label_Not_failed:
-	movl	%eax,-12(%ebp)
+Label_memery_Not_Failed:
+	movl	%eax,Pptr               
 	movl	$0,-8(%ebp)
 	jmp		Label_for_condition1
 
@@ -69,12 +78,12 @@ Label_for_statement1:
 	call	printf
 	addl	$8,%esp
 
-	movl	-8(%ebp),%eax
-	movl	-12(%ebp),%ebx
-	leal    (%ebx,%eax,4),%ecx	#&pptr[iCnt]
-	pushl	%ecx
+	movl 	-8(%ebp),%eax   		    #index
+	movl 	Pptr,%ebx				    #pointer address 
+	Leal 	(%ebx,%eax,4),%ecx		    #scanf("%",Pptr[iCnt]);
+	pushl 	%ecx					    #value ch Address
 	pushl	$msg_main_input_elements_scan
-	call	scanf
+	call 	scanf
 	addl	$8,%esp
 
 	addl	$1,-8(%ebp)
@@ -84,7 +93,7 @@ Label_for_condition1:
 	movl	-8(%ebp),%eax
 	movl	-4(%ebp),%edx
 	cmpl	%edx,%eax
-	jl		Label_for_statement1
+	jl		Label_for_statement1	
 
 	pushl	$msg_main_your_elements_in_Array
 	call	printf
@@ -94,28 +103,31 @@ Label_for_condition1:
 	jmp		Label_for_condition2
 
 Label_for_statement2:
-	movl	-12(%ebp),%ebx		#address dynamic Meory ch
-	movl	(%ebx,%eax,4),%ecx  #ptr[iCnt];
-	
-	pushl	%ecx
-	pushl	%eax
+	movl 	Pptr,%ebx 			    #its Address of pPtr
+	movl	(%ebx,%eax,4),%ecx	    #printf("%d",ptr[iCnt]); %eax iCounter
+	pushl	%ecx				    #element
+	pushl	%eax			    	#index
 	pushl	$msg_main_output_elements_print
-	call	printf
+	call 	printf
 	addl	$12,%esp
 
-	addl	$1,-8(%ebp)
+	addl $1,-8(%ebp)
 
 Label_for_condition2:
 	movl	-8(%ebp),%eax
 	movl	-4(%ebp),%edx
 	cmpl	%edx,%eax
-	jl		Label_for_statement2	
+	jl	Label_for_statement2	
 
-	movl	-12(%ebp),%ebx
-	pushl	%ebx
+	
+    
+    	pushl	Pptr		#Direct Pptr ch Address
 	call	free
 	addl	$4,%esp
-	movl	$0,-12(%ebp)
+    	movl    $0,Pptr		#Direct Pptr Zero push kel
+   
 
 	pushl	$0
-	call	exit
+	call 	exit
+
+
